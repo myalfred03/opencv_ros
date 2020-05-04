a15a62f3ecc4e4106dd4075063728bee56fba35d
myalfred03 <moralesalfredo133@gmail.com>
2020-05-03 18:03:04 -0600

    Test filter blur, threshold, filter2d

--- include/lib_detector_ros.py
+++ include/lib_detector_ros.py
@@ -20,35 +20,54 @@ import numpy as np;
 
 def deNoise (image):
 
-    print ("<< Subscribed to topic /image_raw camera pc")
+	blur = cv2.GaussianBlur(image, (9, 9), 0)
+	print ("<< blur /image")
+	
+	return(blur)
 
+#IMAGE BLURRING
 
+# *@brief Apply gaussian filter to the input image to denoise it
+# *@param inputImage is the frame of a video in which the
+# *@param lane is going to be detected
+# *@return Blurred and denoised image
+ 
 def edgeDetector(image):
 
-    print ("<< Subscribed to topic /image_raw camera pc")
+	gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+	ret1,th1 = cv2.threshold(gray_image,140,255,cv2.THRESH_BINARY)
+
+	
+	kernel = np.ones((5,5),np.float32)/25
+	dst = cv2.filter2D(th1,-1,kernel)
+
+
+	print ("<< gray image")
+	return(dst)
+
 
 
 def mask(image):
 
-    print ("<< Subscribed to topic /image_raw camera pc")
+	print ("<< Subscribed to topic /image_raw camera pc")
 
 def houghLines(image):
 
-    print ("<< Subscribed to topic /image_raw camera pc")
+	print ("<< Subscribed to topic /image_raw camera pc")
 
 def lineSeparation(image):
 
-    print ("<< Subscribed to topic /image_raw camera pc")
+	print ("<< Subscribed to topic /image_raw camera pc")
 
 
 def regression(image):
 
-    print ("<< Subscribed to topic /image_raw camera pc")
+	print ("<< Subscribed to topic /image_raw camera pc")
 
 def predictTurn(image):
 
-    print ("<< Subscribed to topic /image_raw camera pc")
+	print ("<< Subscribed to topic /image_raw camera pc")
 
 def plotLane(image):
 
-    print ("<< Subscribed to topic /image_raw camera pc")
+	print ("<< Subscribed to topic /image_raw camera pc")
--- src/detector_ros.py
+++ src/detector_ros.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python    
 
 if __name__ == '__main__' and __package__ is None:
-	from os import sys, path
-	sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 import sys
 import rospy
@@ -21,123 +21,122 @@ from include.lib_detector_ros  import *
 
 class LaneDetector:
 
-	def __init__(self, cap):
+    def __init__(self, cap):
 
-		self.cap = cap
+        self.cap = cap
 
-		print (">> Publishing image to topic image_blob")
-		self.image_pub = rospy.Publisher("/lane/image_lane",Image,queue_size=1)
-		self.mask_pub = rospy.Publisher("/lane/image_mask",Image,queue_size=1)
-		self.video_pub = rospy.Publisher("/lane/video",Image,queue_size=1)
+        print (">> Publishing image to topic image_blob")
+        self.image_pub = rospy.Publisher("/lane/image_lane",Image,queue_size=1)
+        self.mask_pub = rospy.Publisher("/lane/image_mask",Image,queue_size=1)
+        self.video_pub = rospy.Publisher("/lane/video",Image,queue_size=1)
 
-		print (">> Publishing position to topic point_blob")
-		self.blob_pub  = rospy.Publisher("/lane/point_lane",Point,queue_size=1)
+        print (">> Publishing position to topic point_blob")
+        self.blob_pub  = rospy.Publisher("/lane/point_lane",Point,queue_size=1)
 
-		self.bridge = CvBridge()
-		self.image_sub = rospy.Subscriber("/image_raw",Image,self.callback)
-		print ("<< Subscribed to topic /image_raw camera pc")
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/image_raw",Image,self.callback)
+        print ("<< Subscribed to topic /image_raw camera pc")
 
-		self.read()
+        self.read()
 
-	def read(self):
-	 
-		try:
+    def read(self):
+     
+        try:
 
-			if not self.cap.isOpened():
-				rospy.logerr('Failed to open file')
-				exit(1)
-			while self.cap.isOpened():
+            if not self.cap.isOpened():
+                rospy.logerr('Failed to open file')
+                exit(1)
+            while self.cap.isOpened():
 
-				ret, frame = self.cap.read()
-				gray_image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-				canny_image = cv2.Canny(gray_image, 160, 210)
-				self.video_pub.publish(self.bridge.cv2_to_imgmsg(canny_image, "8UC1")) #8UC1   bgr8
-				#if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
-		except CvBridgeError as e:
-			print(e)
+                ret, frame = self.cap.read()
+                blur = deNoise(frame)
+                gray_image = edgeDetector(blur)
+               # canny_image = cv2.Canny(gray_image, 160, 210)
+                self.video_pub.publish(self.bridge.cv2_to_imgmsg(gray_image, "8UC1")) #8UC1   bgr8
+                #if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except CvBridgeError as e:
+            print(e)
 
-	def callback(self,data):
-		try:
-			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-			print ("subscribe video camera")
+    def callback(self,data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            print ("subscribe video camera")
 
-			gray_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
-			canny_image = cv2.Canny(gray_image, 160, 210)
-			
-		except CvBridgeError as e:
-			print(e)
+            gray_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
+            canny_image = cv2.Canny(gray_image, 160, 210)
+            
+        except CvBridgeError as e:
+            print(e)
 
-		self.read()
-
-
-			# try:
-			# 	self.cap = cv2.VideoCapture('project_video.mp4')
-			# 	while self.cap.isOpened():
-			# 		print ("reading video")
-
-			# 		ret, frame = self.cap.read()
-			# 		try:
-			# 			self.video_pub.publish(self.bridge.cv2_to_imgmsg(frame, "8UC1"))
-
-			# 		except CvBridgeError as e:
-			# 			print(e)  	
-
-			# except CvBridgeError as e:
-			# 	print(e)
-
-			#--- Assuming image is 320x240
+        self.read()
 
 
-		 
-		# try:
-		# 	#self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-		# 	#self.mask_pub.publish(self.bridge.cv2_to_imgmsg(mask, "8UC1"))
-		# 	self.video_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "8UC1"))
+            # try:
+            #   self.cap = cv2.VideoCapture('project_video.mp4')
+            #   while self.cap.isOpened():
+            #       print ("reading video")
 
-		# except CvBridgeError as e:
-		# 	print(e)            
+            #       ret, frame = self.cap.read()
+            #       try:
+            #           self.video_pub.publish(self.bridge.cv2_to_imgmsg(frame, "8UC1"))
+
+            #       except CvBridgeError as e:
+            #           print(e)    
+
+            # except CvBridgeError as e:
+            #   print(e)
+
+            #--- Assuming image is 320x240
+
+
+         
+        # try:
+        #   #self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+        #   #self.mask_pub.publish(self.bridge.cv2_to_imgmsg(mask, "8UC1"))
+        #   self.video_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "8UC1"))
+
+        # except CvBridgeError as e:
+        #   print(e)            
 
 
 # while cap.isOpened():
-# 	print ("reading video")
+#   print ("reading video")
 
-# 	ret, frame = cap.read()
+#   ret, frame = cap.read()
 
 def main(args):
-	rospy.init_node('lane_detector', anonymous=True)
-	rospack = rospkg.RosPack()
-	filevideo = rospack.get_path('opencv_ros') + '/src/project_video.mp4'
+    rospy.init_node('lane_detector', anonymous=True)
+    rospack = rospkg.RosPack()
+    filevideo = rospack.get_path('opencv_ros') + '/src/project_video.mp4'
 
-	img = np.zeros((3,3), dtype=np.uint8)
-	img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-	cap = cv2.VideoCapture(filevideo)
+    img = np.zeros((3,3), dtype=np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-	# if not cap.isOpened():
-	# 	rospy.logerr('Failed to open file')
-	# 	exit(1)
-	# while cap.isOpened():
+    cap = cv2.VideoCapture(filevideo)
 
-	# 	ret, frame = cap.read()
+    # if not cap.isOpened():
+    #   rospy.logerr('Failed to open file')
+    #   exit(1)
+    # while cap.isOpened():
 
-	# 	try:
-	# 		rospy.spin()
-	# 	except KeyboardInterrupt:
-	# 		print("Shutting down")
-	ic = LaneDetector(cap)
+    #   ret, frame = cap.read()
 
-	# 	if cv2.waitKey(1) & 0xFF == ord('q'):
-	# 		break
-	#ic.run()
-	try:
-		rospy.spin()
-	except KeyboardInterrupt:
-		print("Shutting down")
+    #   try:
+    #       rospy.spin()
+    #   except KeyboardInterrupt:
+    #       print("Shutting down")
+    ic = LaneDetector(cap)
 
-	cv2.destroyAllWindows()
+    #   if cv2.waitKey(1) & 0xFF == ord('q'):
+    #       break
+    #ic.run()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-	main(sys.argv)
-
-
-
+    main(sys.argv)
